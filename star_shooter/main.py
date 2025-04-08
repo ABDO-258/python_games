ffe849cabfc9aeea3200eb5413a8306bb8fe6cfd
karbal <abdoraja2005@gmail.com>
2025-04-08 04:44:18 +0100

    add multiple rectangle and learn how to move the player from left to right

--- star_shooter/main.py
+++ star_shooter/main.py
@@ -8,6 +8,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode( (WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('space shooter')
 running = True
+clock = pygame.time.Clock()
 
 
 # plain surface
@@ -19,12 +20,20 @@ x = 100
 pathimg = join('images', 'player.png')
 print(pathimg)
 player_surf = pygame.image.load(pathimg).convert_alpha()
-starimgpath = join('images', 'star.png')
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = 1
 
+starimgpath = join('images', 'star.png')
 star_surf = pygame.image.load(starimgpath).convert_alpha()
 star_position = [(random.randint(0,WINDOW_WIDTH), random.randint(0,WINDOW_HEIGHT)) for i in range(20)]
 
+meteor_surf = pygame.image.load(join ('images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surf = pygame.image.load(join ('images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (WINDOW_WIDTH - 20, WINDOW_HEIGHT - 20))
 while running:
+    clock.tick(24)
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,8 +45,19 @@ while running:
     display_surface.fill(color='darkgray')
     for pos in star_position:
         display_surface.blit(star_surf, pos)
-    x += 0.1
-    display_surface.blit(player_surf, (x,150))
+   
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+    # player movement
+    player_rect.x += player_direction * 0.4
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+
+    # if player_rect.right < WINDOW_WIDTH:
+    #     player_rect.left += 0.2
+    display_surface.blit(player_surf, player_rect)
+    
     pygame.display.flip()
 
 pygame.quit()
